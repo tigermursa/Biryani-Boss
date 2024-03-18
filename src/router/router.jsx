9b29f309e8aa2ae0374761e047049a817fe9c96a
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-19 02:14:41 +0600

    "implement the react router"

---
+++ src/router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!hahah</div>,
+  },
+]);
+
+export default router;
